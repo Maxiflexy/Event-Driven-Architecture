ad22cb8b7cd6ad5c610267f43c2c2797bbc4296c
Maxiflexy <onyemax247@gmail.com>
2025-02-03 13:14:31 +0100

    created email consumer impl

---
+++ email-service/src/main/java/com/maxiflexy/emailservice/kafka/OrderConsumer.java
@@ -0,0 +1,18 @@
+package com.maxiflexy.emailservice.kafka;
+
+import com.maxiflexy.basedomains.dto.OrderEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class OrderConsumer {
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consumeMessage(OrderEvent event){
+        log.info("Order event received in email service, {}", event.toString());
+
+        // save the event data to a database
+    }
+}
